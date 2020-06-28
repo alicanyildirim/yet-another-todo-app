7e863f6ae9dab966123a0c9a6071ed6f37185c85
Alican Yıldırım <alicanyildirim96@outlook.com>
2020-06-28 15:37:54 +0300

    at 3:20

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TodoItem from "./components/TodoItem.js"
-function App() {
-  return (
-    <div className="todo-list">
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-    </div>
-  );
+import TodoItem from "./components/TodoItem"
+import todosData from "./components/todosData"
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: todosData
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if(todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+      return {
+        todos: updatedTodos
+      }
+    })
+    console.log("Changed", id)
+  }
+  render() {
+    const todoItems =  this.state.todos.map(item => <TodoItem key={item.id} item={item} 
+      handleChange={this.handleChange}/>)
+    return (
+  <div className="todo-list">
+        {todoItems}
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/jokesData.js
@@ -0,0 +1,28 @@
+const jokesData = [
+    {
+        id: 1,
+        question:  "My wife told me to stop acting like a flamingo.",
+        punchline: "I had to put my foot down."
+    },
+    {
+        id: 2,
+        question:  "What is Whitney Houston’s favorite type of coordination? ",
+        punchline: "HAAANNNNND EYEEEEEEE."
+    },
+    {
+        id: 3,
+        question:  "Why are gay people always smiling?",
+        punchline: "Because they can’t keep a straight face."
+    },
+    {
+        id: 4,
+        question:  "Why did David Hasselhoff change his name to “The Hoff?”",
+        punchline: "It’s less hassle."
+    },
+    {
+        id: 5,
+        punchline: "A man enters a pun contest in his local newspaper. He sends in ten puns, hoping at least one of them would win, but unfortunately, no pun in ten did."
+    },
+]
+
+export default jokesData
